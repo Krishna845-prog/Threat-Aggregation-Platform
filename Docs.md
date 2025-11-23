f39cd2fe9a09c8ef5134284c690030262d9d42f9
Krishna Iyer <154890699+Krishna845-prog@users.noreply.github.com>
2025-11-23 19:52:07 +0530

    Add files via upload

---
+++ Docs.md
@@ -0,0 +1,14 @@
+## Need to add documentation
+
+### Links
+
+- http://wn6vonooq6fggjdgyocp7bioykmfjket7sbp47cwhgubvowwd7ws5pyd.onion/
+- https://ransomwatch.telemetry.ltd/#/
+- https://www.ransomlook.io/
+- https://www.ransomware.live/
+
+## News Updates
+
+- god damn fbi toke down 3 sites that I was scrapping
+- Onni forums has been taken down by some law enforcement, no claims for the take down have been made so far
+- https://www.bleepingcomputer.com/news/security/fbi-seizes-crackedio-nulledto-hacking-forums-in-operation-talent/
---
+++ start.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+Green="\e[32m"
+Red="\033[0;31m"
+NC='\033[0m'
+
+if [ "$1" = "stop" ]; then
+    echo "${Red}Stopping All Containers${NC}"
+    sudo docker compose down --rmi local
+    echo "${Red}Stopping Dashboard${NC}"
+    kill "$(lsof -t -i:3000)"
+    kill "$(lsof -t -i:3001)"
+    echo "${Red}Stopping APIs${NC}"
+    kill "$(lsof -t -i:8000)"
+    kill "$(lsof -t -i:8010)"
+    kill "$(lsof -t -i:8002)"
+    kill "$(lsof -t -i:8006)"
+    kill "$(lsof -t -i:8004)"
+    kill "$(lsof -t -i:8008)"
+    kill "$(lsof -t -i:8009)"
+    echo "${Red}Stopping Middlewares${NC}"
+    kill "$(lsof -t -i:9000)"
+    echo "${Red}Stopping Main Site${NC}"
+    kill "$(lsof -t -i:5173)"
+    exit 0
+fi
+
+echo "${Green}Starting Admin Panel${NC}"
+
+cd admin/frontend || exit
+
+# npm run dev &
+
+cd ../backend || exit
+
+# node app.js &
+
+cd ../.. || exit
+
+echo "${Green}Starting API's${NC}"
+
+cd api || exit
+
+# ./starter.sh &
+
+cd .. || exit
+
+echo "${Green}Starting Middleware${NC}"
+
+cd middleware || exit
+
+# python3 api.py &
+
+cd .. || exit
+
+echo "${Green}Starting Main Site${NC}"
+
+cd frontend || exit
+
+# npm run dev &
+
+echo "${Green}Starting All Containers${NC}"
+sudo docker compose up -d
+
+cd ..
